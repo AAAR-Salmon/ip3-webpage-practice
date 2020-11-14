0eb73a0db260c5fe0900b017fcdad6256b8c70ac
AAAR Salmon <TenzikuJohn@gmail.com>
2020-11-14 14:48:37 +0900

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+let answer = {};
+let radios = document.querySelectorAll('.radio');
+window.addEventListener('load', () => {
+	if (localStorage.webpagePractice != void(0)) {
+		answer = JSON.parse(localStorage.webpagePractice);
+	}
+
+	for (var key in answer) {
+		if (answer.hasOwnProperty(key)) {
+			radios.forEach((v, i) => {
+				if (v.dataset.question === key && v.dataset.value === answer[key]) {
+					v.dataset.selected = true;
+				}
+			});
+		}
+	}
+
+	radios.forEach((v, i) => {
+		console.log(i, v.dataset.question, v.dataset.value);
+		v.addEventListener('click', () => {
+			radios.forEach((w, j) => {
+				w.dataset.selected = false;
+			});
+			v.dataset.selected = true;
+			answer[v.dataset.question] = v.dataset.value;
+		}, false);
+	});
+}, false);
+
+window.addEventListener('unload', () => {
+	localStorage.webpagePractice = JSON.stringify(answer);
+}, false);
